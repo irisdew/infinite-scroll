fa0c738eecd3705afb8da0e4b35afd5a975d3e80
irisdew <ycleardew@gmail.com>
2021-06-30 16:03:41 +0900

    :sparkles: article을 관리하는 context 생성

---
+++ src/context/ArticleContext.js
@@ -0,0 +1,9 @@
+import React, { useState, createContext } from "react";
+
+export const ArticleContext = createContext();
+
+export const ArticleProvider = ({ children }) => {
+  const [article, setArticle] = useState([]);
+
+  return <ArticleContext.Provider value={{ article, setArticle }}>{children}</ArticleContext.Provider>;
+};
