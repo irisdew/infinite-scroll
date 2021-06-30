823db772f81d53bfa7839d19fab3a4da04c83b03
irisdew <ycleardew@gmail.com>
2021-06-30 17:33:49 +0900

    :recycle: Main 페이지로 분리

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import React, { useState, useEffect } from "react";
-import styled from "styled-components";
+import React from "react";
 import { ArticleProvider } from "./context/ArticleContext";
-import Board from "./components/Board";
-import Search from "./components/Search";
+import Main from "./pages/Main";
 
 function App() {
   return (
@@ -17,27 +15,10 @@ function App() {
             height: "13px",
           }}
         ></div>
-        <Container>
-          <h1>Solidarite Board</h1>
-          <p>게시물을 검색해보세요</p>
-          <br />
-          <Search />
-          <br />
-          <Board />
-        </Container>
+        <Main />
       </ArticleProvider>
     </>
   );
 }
 
 export default App;
-
-const Container = styled.div`
-  width: 70%;
-  margin: 50px auto;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  overflow: auto;
-`;
---
+++ src/pages/Main.js
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+import Board from "../components/Board";
+import Search from "../components/Search";
+
+function Main() {
+  return (
+    <Container>
+      <h1>Solidarite Board</h1>
+      <p>게시물을 검색해보세요</p>
+      <br />
+      <Search />
+      <br />
+      <Board />
+    </Container>
+  );
+}
+
+export default Main;
+
+const Container = styled.div`
+  width: 70%;
+  margin: 50px auto 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  overflow: auto;
+`;
